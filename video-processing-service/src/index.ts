e11d4a22934b5909fb993bdabd780f5cf0c85961
Devansh Sharma <devansh.sharma9100@gmail.com>
2025-09-20 18:09:00 +0530

    Added videos to firestore

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { convertVideo, deleteProcessedVideo, deleteRawVideo, downloadRawVideo, setupDirectories, uploadProcessedVideo } from "./storage";
+import { isVideoNew, setVideo } from "./firestore";
 
 setupDirectories();
 
@@ -22,6 +23,17 @@ app.post("/process-video", async (req, res) => {
 
     const inputFileName = data.name;
     const outputFileName = `processed-${inputFileName}`;
+    const videoId = inputFileName.split('.')[0];
+
+    if (!isVideoNew(videoId)) {
+        res.status(400).send('Bad Request: video already processing or processed.');
+    } else {
+        await setVideo(videoId, {
+            id: videoId,
+            uid: videoId.split('-')[0],
+            status: 'processing'
+        });
+    }
 
     // Download the raw video from cloud storage
     await downloadRawVideo(inputFileName);
@@ -40,6 +52,11 @@ app.post("/process-video", async (req, res) => {
 
     // Upload the processed video to cloud storage
     await uploadProcessedVideo(outputFileName);
+
+    await setVideo(videoId, {
+        filename: outputFileName,
+        status: 'processed'
+    });
 
     await Promise.all([
         deleteRawVideo(inputFileName),
